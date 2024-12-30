4aef53cfb1bc0550d762c06fa8ee1d3db9a6d635
AnnHouseflow <annchou.houseflow@gmail.com>
2024-12-30 13:59:33 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'photo-gallery_20241230',
+  webDir: 'www'
+};
+
+export default config;
