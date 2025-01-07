3c9790f355a8fe465d096c4dbb9e946b3c5ff63d
AnnHouseflow <annchou.houseflow@gmail.com>
2025-01-07 13:49:40 +0800

    feat: 1. 增加 PWA 功能。2. 完成 firebase PWA 佈署。

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
+import "./services/firebase.config";
+
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
   
